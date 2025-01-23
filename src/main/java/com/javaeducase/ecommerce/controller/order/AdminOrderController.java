a42966b58f66ecb53bb6121ac80628ffab32ff55
Victor Fedyanin <fedyanin.v.v@yandex.ru>
2025-01-23 12:24:41 +0300

    Swagger: - Add descriptions to all Order-related controllers

--- src/main/java/com/javaeducase/ecommerce/controller/order/AdminOrderController.java
+++ src/main/java/com/javaeducase/ecommerce/controller/order/AdminOrderController.java
@@ -1,7 +1,13 @@
 package com.javaeducase.ecommerce.controller.order;
 
 import com.javaeducase.ecommerce.dto.order.OrderDTO;
+import com.javaeducase.ecommerce.handler.CustomErrorResponse;
 import com.javaeducase.ecommerce.service.order.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +24,22 @@ public class AdminOrderController {
 
     private final OrderService orderService;
 
+    @Operation(summary = "Get orders by user ID",
+            description = "Retrieve all orders of a user by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Successfully retrieved the list of orders",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderDTO.class))
+            }),
+            @ApiResponse(responseCode = "404",
+                    description = "User not found",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CustomErrorResponse.class))
+            })
+    })
     @GetMapping("/{userId}")
     public ResponseEntity<List<OrderDTO>> getUserOrders(@PathVariable Long userId) {
         List<OrderDTO> orders = orderService.getUserOrders(userId);
--- src/main/java/com/javaeducase/ecommerce/controller/order/OrderController.java
+++ src/main/java/com/javaeducase/ecommerce/controller/order/OrderController.java
@@ -2,8 +2,14 @@ package com.javaeducase.ecommerce.controller.order;
 
 import com.javaeducase.ecommerce.dto.order.OrderDTO;
 import com.javaeducase.ecommerce.dto.order.RequestOrderDTO;
+import com.javaeducase.ecommerce.handler.CustomErrorResponse;
 import com.javaeducase.ecommerce.service.order.OrderService;
 import com.javaeducase.ecommerce.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +25,45 @@ public class OrderController {
     private final OrderService orderService;
     private final UserService userService;
 
-    @GetMapping
+
+    @Operation(summary = "Get current user's orders",
+            description = "Retrieve all orders for the currently authenticated user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Successfully retrieved orders",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderDTO.class))
+            })
+    })
+    @GetMapping("/me")
     public ResponseEntity<List<OrderDTO>> getOrders() {
         return ResponseEntity.ok(orderService.getUserOrders(userService.getCurrentUser().getId()));
     }
 
+
+    @Operation(summary = "Create a new order",
+            description = "Create a new order from the user's cart")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Order successfully created",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderDTO.class))
+            }),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid order data",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CustomErrorResponse.class))
+            }),
+            @ApiResponse(responseCode = "404",
+                    description = "Cart or other related entity not found",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CustomErrorResponse.class))
+            })
+    })
     @PostMapping("/create")
     public ResponseEntity<OrderDTO> createOrder(@RequestBody RequestOrderDTO requestOrderDTO) {
         OrderDTO orderDTO = orderService.createOrder(requestOrderDTO);
